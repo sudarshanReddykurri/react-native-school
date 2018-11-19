0c4a33540d21c9c23a06e25adac4c7cce2eef6c1
Spencer Carli <spencercarli@gmail.com>
2018-11-19 11:03:31 -0600

    Stuff

--- courses/form-component-library/App.js
+++ courses/form-component-library/App.js
@@ -1,21 +1,95 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  View
+} from "react-native";
+import { createStackNavigator } from "react-navigation";
+import get from "lodash/get";
+
+// import LoginReference from "./lessons/LoginReference";
+// import RegisterReference from "./lessons/RegisterReference";
+
+// import { SelectList } from "./components/Form";
+
+const initialRouteName = "Index";
+
+export const routeConfig = {
+  // LoginReference: {
+  //   screen: LoginReference,
+  //   navigationOptions: {
+  //     title: "Login Reference"
+  //   }
+  // },
+  // RegisterReference: {
+  //   screen: RegisterReference,
+  //   navigationOptions: {
+  //     title: "Register Reference"
+  //   }
+  // },
+  // SelectList: {
+  //   screen: SelectList,
+  //   navigationOptions: ({ navigation }) => ({
+  //     headerTitle: navigation.getParam("title", "Select")
+  //   })
+  // }
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  scrollview: {
+    backgroundColor: "#fff",
+    marginVertical: 20,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderTopWidth: StyleSheet.hairlineWidth,
+    borderColor: "#ddd"
   },
+  itemContainer: {
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: "#ddd"
+  },
+  item: {
+    paddingHorizontal: 16,
+    paddingVertical: 12
+  },
+  title: {
+    fontSize: 16,
+    color: "#444"
+  }
 });
 
-export default class App extends React.Component {
+class LessonList extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <ScrollView contentContainerStyle={styles.scrollview}>
+        {Object.keys(routeConfig).map(key => (
+          <TouchableOpacity
+            onPress={() => this.props.navigation.navigate(key)}
+            style={styles.itemContainer}
+            key={key}
+          >
+            <View style={styles.item}>
+              <Text style={styles.title}>
+                {get(routeConfig[key], "navigationOptions.title", key)}
+              </Text>
+            </View>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
     );
   }
 }
+
+export default createStackNavigator(
+  {
+    ...routeConfig,
+    Index: {
+      screen: LessonList,
+      navigationOptions: {
+        title: "Form Component Library - Index"
+      }
+    }
+  },
+  { initialRouteName }
+);
